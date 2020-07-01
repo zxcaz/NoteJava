ebf9b501790d129e14c4ce061eb10f86ba6eae30
zhangxi <634481879@qq.com>
2020-07-01 12:00:55 +0800

    Initial commit

---
+++ app/src/main/java/com/zx/note/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zx.note;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author zhangxi
+ * @date 2020/7/1
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
